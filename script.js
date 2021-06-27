57d5c4bfdf5b0323ec57389b5026794ac5fcb15d
cesque <euqsec@gmail.com>
2021-06-27 17:01:39 +0100

    update new tab page for PoE offseason

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ let app = new Vue({
             ],
             links: [
                 {
-                    href: `https://www.reddit.com/r/pathofexile`,
-                    title: `path of exile reddit`,
+                    href: `https://www.reddit.com/`,
+                    title: `reddit`,
                     big: true,
                 },
                 {
@@ -42,84 +42,21 @@ let app = new Vue({
                     big: true,
                 },
                 {
-                    href: `https://www.pathofexile.com/forum/view-forum/news`,
-                    title: `poe official forum: announcements`,
+                    href: `https://store.steampowered.com/`,
+                    title: `steam store`,
                 },
                 {
-                    href: `https://www.craftofexile.com/`,
-                    title: `craftofexile`,
-                },
-                {
-                    href: `https://poedb.tw/`,
-                    title: `poedb.tw`,
-                },
-                {
-                    href: `https://www.youtube.com/watch?v=cUY6jgZ0zic&t=12s`,
-                    title: `detonate dead build`,
+                    href: `https://www.reddit.com/r/all/`,
+                    title: `/r/all`,
                     big: true,
                 },
                 {
-                    href: `https://www.pathofexile.com/forum/view-thread/2839382`,
-                    title: `flicker strike build`,
-                    big: true,
+                    href: `https://www.wanikani.com/`,
+                    title: `wanikani`,
                 },
                 {
-                    href: `https://poe-leveling.netlify.app/`,
-                    title: `poe leveling guide`,
-                },
-                {
-                    href: `https://www.pathofexile.com/trade/search/Ritual`,
-                    title: `poe trade`,
-                    big: true,
-                },
-                {
-                    href: `https://www.pathofexile.com/trade/exchange/Ritual`,
-                    title: `poe bulk trade`,
-                },
-                {
-                    href: `http://siveran.github.io/calc.html`,
-                    title: `vorici chromatic calculator`,
-                },
-                {
-                    href: `https://poe.ninja/`,
-                    title: `poe.ninja`,
-                    big: true,
-                },
-                {
-                    href: `https://filterblade.xyz/`,
-                    title: `filterblade`,
-                },
-                {
-                    href: `https://pathofexile.gamepedia.com/`,
-                    title: `poe wiki`,
-                },
-                {
-                    href: `https://www.poelab.com/`,
-                    title: `poelab`,
-                },
-                {
-                    href: `https://www.poelab.com/syndicate-cheatsheet/`,
-                    title: `poelab syndicate cheatsheet`,
-                },
-                {
-                    href: `https://wraeclast.com/wp-content/uploads/2020/09/reward_table.jpg`,
-                    title: `heist rewards`,
-                },
-                {
-                    href: `https://pathofexile.gamepedia.com/List_of_cluster_jewel_notable_passive_skills`,
-                    title: `poe wiki list of cluster jewel notables`,
-                },
-                {
-                    href: `https://poe-antiquary.xyz/`,
-                    title: `poe antiquary (historic prices)`,
-                },
-                {
-                    href: `https://www.reddit.com/r/pathofexile/comments/hbmims/guide_stepbystep_visual_guide_on_atlas/`,
-                    title: `atlas progression guide`,
-                },
-                {
-                    href: `https://poeprofit.com/`,
-                    title: `poe profit`,
+                    href: `https://jisho.org/`,
+                    title: `jisho`,
                 },
             ]
         }
@@ -138,6 +75,9 @@ let app = new Vue({
         },
         empty: function() {
             return this.columns - (this.length % this.columns)
+        },
+        randomColor: function() {
+            return this.colors[Math.floor(Math.random() * this.colors.length)]
         }
     },
     methods: {
